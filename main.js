167f2c3adbba0b9984d58126cd4f0d3e54307f7f
vtnboolean <vnthuanx@gmail.com>
2022-06-30 17:11:16 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,155 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "is_liked" : true,
+        "created": "2022-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "is_liked" : false,
+        "created": "2022-06-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "is_liked" : true,
+        "created": "2022-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "is_liked" : false,
+        "created": "2022-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Stefano Tortellini",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+         "is_liked" : false,
+        "created": "2022-03-05"
+    },
+    {
+        "id": 6,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=536",
+        "author": {
+            "name": "Luigia Micca",
+            "image": "https://unsplash.it/300/300?image=33"
+        },
+        "likes": 95,
+         "is_liked" : true,
+        "created": "2022-02-02"
+    },
+    {
+        "id": 7,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=531",
+        "author": {
+            "name": "Grace Hunterdan",
+            "image": "https://unsplash.it/300/300?image=59"
+        },
+        "likes": 95,
+         "is_liked" : false,
+        "created": "2022-02-01"
+    },
+    {
+        "id": 8,
+        "content": "Ao! Che nun ce lo sai che io so l'unico post scritto in romanesco de tutta sta lista autogenerata! Dico e scrivo nummeri da quanno so nato, ce manca pure che me faccio un post autogennerato!",
+        "media": "https://unsplash.it/600/400?image=554",
+        "author": {
+            "name": "Mario Di Nio",
+            "image": "null"
+        },
+        "likes": 95,
+         "is_liked" : true,
+        "created": "2021-12-11"
+    }
+];
+
+const postsWrapper = document.querySelector('.posts-list');
+const btn = document.getElementById('like-button');
+
+posts.forEach((element,index)=>{
+    
+createImage(element, index);
+
+    
+
+});
+
+function createImage(element,index){
+    let currentPost= document.createElement('div');
+    currentPost.innerHTML= `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${element.author.name}</div>
+                <div class="post-meta__time">${element.created}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${element.content}</div>
+    <div class="post__image">
+        <img src="${element.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>`
+
+postsWrapper.append(currentPost);
+
+}
+
+
+
+btn.addEventListener('click', function onClick() {
+  btn.target.style.backgroundColor = 'salmon';
+  btn.target.style.color = 'white';
+});
